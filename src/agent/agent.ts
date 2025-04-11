a12e0945c5661efbb1ebeb28f2797af13ab63e06
bob <veltmeyer.s@gmail.com>
2025-04-11 10:02:54 -0700

    AgentService

--- src/agent/agent.ts
+++ src/agent/agent.ts
@@ -10,36 +10,6 @@ interface CustomFunctionTool extends FunctionTool {
     functionToCall: (...args: any[]) => any;
 }
 
-// {
-//         type: "function",
-//         name: "getChallengerSummonersForSoloQ",
-//         description:
-//         "Get a list of summoners in challenger league for league of legends solo queue",
-//         // parameters: {
-//         //   type: "object",
-//         //   additionalProperties: false,
-//         // },
-//         strict: false,
-
-//         // parameters: {
-//         //   type: "object",
-//         //   properties: {
-//         //     aNumber: {
-//         //       type: "number",
-//         //       description: "Choose a number between 1 and 59",
-//         //     },
-//         //     aString: {
-//         //       type: "string",
-//         //       description: "A single sentence describing your mood",
-//         //     },
-//         //   },
-//         //   required: ["aNumber", "aString"],
-//         //   additionalProperties: false,
-//         // },
-//         functionToCall: () =>
-//         LeagueApi.getChallengerSummonersByQueue("RANKED_SOLO_5x5"),
-//     }
-
 export class AgentService {
     #functionTools: Map<string, CustomFunctionTool> = new Map();
 
